3aab59eda3e8f6a1b1730a47bf8c37883d2cd02a
khushal <khushalv965@gmail.com>
2024-08-05 12:37:39 +0530

    notes taking app using backend

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    fs.readdir(`./files`, function (err, files) {
+        res.render("index", { files: files });
+    })
+});
+app.post('/create', function (req, res) {
+    fs.writeFile(`./files/${req.body.name.split(' ').join("")}.txt`, req.body.title, function (err) {
+        res.redirect("/");
+    });
+
+});
+
+app.listen(3000);
